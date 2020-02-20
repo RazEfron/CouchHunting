21c3027b4b727fa8043b858d9a1c1a5f19143503
Raz <razefron01@gmail.com>
2020-02-20 18:49:28 -0500

    deploying to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -105,12 +105,10 @@ alex_profile = Profile.create!({
                                 location_id: nyc.id, 
                                 hosting_status: " not accepting guests", 
                                 date_of_birth: "09/11/1985", 
-<<<<<<< HEAD
-<<<<<<< HEAD
                                 gender: "other", 
                                 occupation: "hydrotherapyst", 
                                 about_me: "I'm nice!!", 
-=======
+
                                 gender: "male", 
                                 occupation: "musician", 
                                 about_me: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -172,7 +170,6 @@ gilad_profile = Profile.create!({
                                 hosting_status: "accepting guests", 
                                 date_of_birth: "22/07/1994", 
                                 gender: "other", 
-=======
                                 gender: "male", 
                                 occupation: "musician", 
                                 about_me: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.

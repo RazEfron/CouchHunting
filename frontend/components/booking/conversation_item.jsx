03b7aea6bfc5a5b06b483f2c6e9630e4b43961dc
Raz <razefron01@gmail.com>
2020-04-05 11:23:38 -0400

    unread dot displaying

--- frontend/components/booking/conversation_item.jsx
+++ frontend/components/booking/conversation_item.jsx
@@ -21,47 +21,55 @@ class ConversationItem extends React.Component {
         if (this.props.message) {
             return (
                 <li 
-                value={this.props.conversation.id} 
-                className="conversation-item" 
-                onClick={(e) => this.clickHandler(e)}>
-                <div>
-                    {/* image name and location */}
-                    <img src={this.props.photo.photoUrl}/>
-                    <div>
-                        <h1>
-                            {this.props.profile.username}
-                        </h1>
-                        <h2>
-                            <a>{`${this.props.currentLocation.city},${this.props.currentLocation.country}`}</a>
-                        </h2>
+                    value={this.props.conversation.id} 
+                    className="conversation-item" 
+                    onClick={(e) => this.clickHandler(e)}
+                >
+                    <div id="left-conversation-item">
+                        {this.props.message.status === "unread" && this.props.message.profile_id !== parseInt(this.props.match.params.profileId, 10) ? (
+                            <span className="unread-dote">1</span>
+                        ) : (
+                                ""
+                            )}
                     </div>
-                </div>
-                    {/* who sent message, last message */}
-                    {this.props.message.profile_id === parseInt(this.props.match.params.profileId, 10) ? (
-                        <div id="conversation-item-right-display">
-                            <h3>
-                                You sent A message to {this.props.profile.username}
-                            </h3>
-                            <p className="my-message">
-                                <span>
-                                    {this.props.message.body}
-                                </span>
-                            </p>
+                    <div>
+                        {/* image name and location */}
+                        <img src={this.props.photo.photoUrl}/>
+                        <div>
+                            <h1>
+                                {this.props.profile.username}
+                            </h1>
+                            <h2>
+                                <a>{`${this.props.currentLocation.city},${this.props.currentLocation.country}`}</a>
+                            </h2>
                         </div>
-                    ) : (
-                        <div id="conversation-item-right-display">
-                            <h3>
-                                {this.props.profile.username} sent you a message
-                            </h3>
-                            <p className="not-my-message">
-                                <span>
-                                    {this.props.message.body}
-                                </span>
-                            </p>
-                        </div>
-                    )}
-            </li>
-        )
+                    </div>
+                        {/* who sent message, last message */}
+                        {this.props.message.profile_id === parseInt(this.props.match.params.profileId, 10) ? (
+                            <div id="conversation-item-right-display">
+                                <h3>
+                                    You sent A message to {this.props.profile.username}
+                                </h3>
+                                <p className="my-message">
+                                    <span>
+                                        {this.props.message.body}
+                                    </span>
+                                </p>
+                            </div>
+                        ) : (
+                            <div id="conversation-item-right-display">
+                                <h3>
+                                    {this.props.profile.username} sent you a message
+                                </h3>
+                                <p className="not-my-message">
+                                    <span>
+                                        {this.props.message.body}
+                                    </span>
+                                </p>
+                            </div>
+                        )}
+                </li>
+            )
         }
         return ""
     }
